5499af9314fd38dc7b0db5f1d96446b4877207b0
Joshua O <amazingjosh101@gmail.com>
2025-02-24 23:46:53 +0000

    problem1.py done

--- problem1.py
+++ problem1.py
@@ -1,8 +1,8 @@
 #We are going to create a function to reverse the order of a string
 
-def reverse_string():
-    phrase = input("Enter a phrase: ")
-    reverse_phrase = phrase[::-1]
-    print(reverse_phrase) 
-reverse_string()    
+def reverse_string(): #create our function
+    phrase = input("Enter a phrase: ")#take input from user to add a phrase (string)
+    reverse_phrase = phrase[::-1]#process of reversing string, we slice it
+    print(reverse_phrase) #finally print out the reversed string
+reverse_string()    #call the function
 
