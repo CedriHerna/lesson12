3eb7b3e3880cb7f9c61d9508274dc58abec92177
Joshua O <amazingjosh101@gmail.com>
2025-02-24 23:37:47 +0000

    Implemented function to reverse string in problem1.py using UCASE

--- problem1.py
+++ problem1.py
@@ -1,1 +1,8 @@
+#We are going to create a function to reverse the order of a string
+
+def reverse_string():
+    phrase = input("Enter a phrase: ")
+    reverse_phrase = phrase[::-1]
+    print(reverse_phrase) 
+reverse_string()    
 
